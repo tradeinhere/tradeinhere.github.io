f8460725eda32d5efff83a69323b3cc2081f765c
Tiwari-shivang <77392891+Tiwari-shivang@users.noreply.github.com>
2021-04-08 13:43:54 +0530

    designed by Tiwari

---
+++ index.js
@@ -0,0 +1,21 @@
+//var header;
+//var stck;
+
+//header=document.getElementById("creat");
+//stck=header.offsetTop;
+
+//stck=header.offsetTop;
+//window.onscrool=function()
+//{
+//  call();
+//}
+
+//function call()
+//{
+  //if(window.pageYoffset > stck)
+  //{
+  //  header.classList.add("stck");
+  //}
+  //else {
+  ////  header.classList.remove("stck");
+  //}
